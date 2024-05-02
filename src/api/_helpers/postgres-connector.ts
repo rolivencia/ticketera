e35ef2d6f341f03fc3e394ea7b49ab170185d08f
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-05-01 22:42:35 -0300

    [#2] - Cambia nombre de cliente

--- src/api/_helpers/postgres-connector.ts
+++ src/api/_helpers/postgres-connector.ts
@@ -5,5 +5,5 @@ import { environment } from './environment';
 const connectionString = environment.database.connectionString;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+export const pgClient = postgres(connectionString, { prepare: false });
+export const db = drizzle(pgClient);
