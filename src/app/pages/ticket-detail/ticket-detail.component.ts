c13c5ba8adfaad05b2ee7789c9d84e3d975b41c8
juanblas09 <jbtschopp@gmail.com>
2024-05-02 16:10:25 -0300

    [#6] - Implementacion de angularx-qrcode para generar los códigos

--- src/app/pages/ticket-detail/ticket-detail.component.ts
+++ src/app/pages/ticket-detail/ticket-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QRCodeModule } from 'angularx-qrcode';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
 	selector: 'ticketera-ticket-detail',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, QRCodeModule],
 	template: `
 		<div class="m-5 grid rounded bg-white p-5 text-center drop-shadow">
 			<div class="flex justify-between">
@@ -17,15 +19,30 @@ import { CommonModule } from '@angular/common';
 				<div class="text-left">18/05/2024 <br> 23:59</div>
 				<div class="text-right"><span class="font-bold">Casa Grande</span><br>Belgrano 3298, Santa Fe</div>
 			</div>
-			<img class="w-[250px] mx-auto" src="https://qrcg-free-editor.qr-code-generator.com/main/assets/images/websiteQRCode_noFrame.png" alt="">
+			<qrcode class="mx-auto" (qrCodeURL)="onChangeURL($event)" [qrdata]="qrString" [width]="256" [errorCorrectionLevel]="'M'"></qrcode>
+			<a [href]="qrCodeDownloadLink" download="qrcode">Download</a>
 			<p class="text-2xl font-bold">TSCHOPP, Juan Blas</p>
 			<p class="text-2xl font-bold">42530052</p>
-			<button class="flex w-full mt-5 bg-success hover:bg-success-dark text-white font-bold py-2 px-4 rounded drop-shadow justify-center">
-				<img class="h-5 mr-1" src="/assets/img/icons/whatsapp-white.svg" alt="" />
-				<span>ENVIAR POR WHATSAPP</span>
-			</button>
+			<a href="https://api.whatsapp.com/send/?phone=549{{phone}}&text=Tu+entrada+para+%2AGUALICHO+FEST%2A%0A%0ALink%3A+{{qrCodeDownloadLink}}&type=phone_number&app_absent=0" target="_blank">
+				<button class="flex w-full mt-5 bg-success hover:bg-success-dark text-white font-bold py-2 px-4 rounded drop-shadow justify-center">
+					<img class="h-5 mr-1" src="/assets/img/icons/whatsapp-white.svg" alt="" />
+					<span>ENVIAR POR WHATSAPP</span>
+				</button>
+			</a>
 		</div>
 	`,
 	styleUrl: './ticket-detail.component.scss',
 })
-export class TicketDetailComponent {}
+export class TicketDetailComponent {
+	public qrString: string = "";
+	public qrCodeDownloadLink: SafeUrl = "";
+	public phone = "3404516946";
+
+	constructor() {
+		this.qrString = crypto.randomUUID();
+	}
+
+	onChangeURL(url: SafeUrl) {
+		this.qrCodeDownloadLink = url;
+	}
+}
