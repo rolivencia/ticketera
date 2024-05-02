12431d8474e202c8e05fdcc5b96e2873a25d93f7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-02 00:19:39 -0300

    [#1] - Agrega schemas para tablas user, seller y ticket

--- src/api/schemas/seller.ts
+++ src/api/schemas/seller.ts
@@ -1,12 +1,15 @@
 // Drizzle
 import { boolean, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+// Schemas
+import { user } from './user';
+
+export const seller = pgTable('seller', {
 	id: serial('id').primaryKey(),
-	firstName: text('first_name'),
-	lastName: text('last_name'),
-	userName: text('user_name'),
-	email: text('email'),
+	description: text('description'),
+	organization: text('organization'),
+	phone: text('phone'),
+	idUser: serial('id_user').references(() => user.id),
 	createdAt: timestamp('created_at'),
 	updatedAt: timestamp('updated_at'),
 	enabled: boolean('enabled'),
