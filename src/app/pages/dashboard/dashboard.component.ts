9c347e0022a4f1f67f6cf977389833efa0c0f100
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-06 13:33:07 -0300

    [#4] - Genera resultados de tabla de tickets mediante consulta a backend

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TicketService } from '../../providers/ticket.service'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 
 @Component({
 	selector: 'ticketera-dashboard',
 	standalone: true,
 	imports: [CommonModule],
 	template: `
+		@if(tickets$ | async; as tickets){
 		<div class="m-5 grid rounded bg-white p-5 text-center drop-shadow">
-			<span class="text-5xl font-bold text-success">4</span>
+			<span class="text-5xl font-bold text-success">{{ tickets.length }}</span>
 			<span class="text-m font-bold">ENTRADAS VENDIDAS</span>
 		</div>
 
@@ -22,17 +25,22 @@ import { CommonModule } from '@angular/common';
 				</thead>
 
 				<tbody class="divide-y divide-gray-200">
-					<!-- <tr *ngFor="let ticket of tickets">
-						<td class="whitespace-nowrap px-4 py-5 font-medium text-gray-900">#{{ ticket.numero }}</td>
-						<td class="whitespace-nowrap px-4 py-5 text-gray-700">{{ ticket.apellido }}, {{ ticket.nombre }}</td>
+				
+
+					<tr *ngFor="let ticket of tickets">
+						<td class="whitespace-nowrap px-4 py-5 font-medium text-gray-900">#{{ ticket.id }}</td>
+						<td class="whitespace-nowrap px-4 py-5 text-gray-700">{{ ticket.lastName }}, {{ ticket.firstName }}</td>
 						<td class="flex justify-between whitespace-nowrap px-4 py-5">
 							<img class="h-5" src="/assets/img/icons/qr-code.svg" alt="" />
 							<img class="h-5" src="/assets/img/icons/whatsapp.svg" alt="" />
 						</td>
-					</tr> -->
+					</tr>
+
 				</tbody>
 			</table>
 		</div>
+		}
+
 		<button
 			class="bg-success hover:bg-success-dark fixed bottom-4 right-4 rounded-full px-4 py-2 font-bold text-white shadow-lg flex"
 		><img class="h-5 mr-1" src="/assets/img/icons/add.svg" alt="" /> GENERAR ENTRADA</button>
@@ -40,5 +48,6 @@ import { CommonModule } from '@angular/common';
 	styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-	
+	private ticketService = inject(TicketService);
+	tickets$ = this.ticketService.getAllTickets().pipe(takeUntilDestroyed());
 }
