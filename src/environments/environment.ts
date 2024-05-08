7cad153c7562e11054e3d1a5edc9fdd9fced719f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-08 00:18:26 -0300

    [#10] - Agrega archivo de environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	production: true,
+	redirectUri: 'https://ticketera-hlxj.onrender.com',
+	domain: 'dev-0wyaah0g.us.auth0.com',
+	clientId: 'fGmUVfnWUFq1uQrtWlvdfzd2abbZyoed',
+};
