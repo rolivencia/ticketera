ee544e7cde652e13fa78aa65ebf7955afd0014e7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-07 22:02:22 -0300

    [#14] - Agrega método para generar request e endpoint /user/email/:email

--- src/app/providers/user.service.ts
+++ src/app/providers/user.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+import { User } from '../interfaces/user.interface'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private readonly prefix = `http://localhost:4200/api/user`;
+
+  private http = inject(HttpClient);
+
+  getByEmail(email: string): Observable<User> {
+    return this.http.get<User>(`${this.prefix}/${email}`)
+  }
 }
