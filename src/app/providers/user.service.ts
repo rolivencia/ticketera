787d3458eaead91c0ac89920a0b06235236e19b4
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-08 00:26:51 -0300

    [TL] - Remueve hardcoding de URLs de APIs

--- src/app/providers/user.service.ts
+++ src/app/providers/user.service.ts
@@ -11,7 +11,7 @@ import { User } from '../interfaces/user.interface'
 })
 export class UserService {
 
-  private readonly prefix = `http://localhost:4200/api/user`;
+  private readonly prefix = `/api/user`;
 
   private http = inject(HttpClient);
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
 	production: true,
 	redirectUri: 'https://ticketera-hlxj.onrender.com',
+	// redirectUri: 'http://localhost:4200',
 	domain: 'dev-0wyaah0g.us.auth0.com',
 	clientId: 'fGmUVfnWUFq1uQrtWlvdfzd2abbZyoed',
 };
