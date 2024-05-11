ce886e7fd9818b3ed2c9d4f6647992d708cf5ec1
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-05-11 03:22:46 -0300

    [HF] - Actualiza environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	redirectUri: 'https://ticketera-theta.vercel.app',
+	redirectUri: 'https://ticketera-gualicho.vercel.app',
 	// redirectUri: 'http://localhost:4200',
 	domain: 'reset-shop.us.auth0.com',
 	clientId: 'QQxwdvYEMgCgxQMAOMAaGUww1hXDn5zI',
