50edd461865392a0095ecae9cf7ab091793c899d
Juan Blas Tschopp <jbtschopp@gmail.com>
2024-05-15 18:05:57 -0300

    [#30] - Attempt to scan QR Code

--- src/app/pages/ticket-redeem/ticket-redeem.component.ts
+++ src/app/pages/ticket-redeem/ticket-redeem.component.ts
@@ -1,33 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ZXingScannerModule  } from '@zxing/ngx-scanner';
+import { ZXingScannerComponent, ZXingScannerModule  } from '@zxing/ngx-scanner';
 
 import { FormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'ticketera-ticket-redeem',
   standalone: true,
   imports: [CommonModule, ZXingScannerModule, FormsModule],
   template: `
-    <div
-      id="scanner-container"
-      class="bg-white"
-    >
-      <zxing-scanner
-        (scanSuccess)="onCodeResult($event)"
-      >
+      <zxing-scanner (scanSuccess)="onScanSuccess($event)">
       </zxing-scanner>
-      <p>	{{result}}</p>
-    </div>
-
-
   `,
   styleUrl: './ticket-redeem.component.scss'
 })
 export class TicketRedeemComponent {
-  result: string = '';
 
-  onCodeResult(result:string){
-    this.result=result
-  }
+onScanSuccess($event: string) {
+  console.log($event);
+}
+
+
 }
