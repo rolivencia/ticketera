b8ec2f768a13c7b0c6cf80c131af96b39cfa63fa
rolivencia <ramiro.olivencia@latch.com>
2024-05-15 20:32:08 -0300

    test3

--- src/app/pages/ticket-redeem/ticket-redeem.component.ts
+++ src/app/pages/ticket-redeem/ticket-redeem.component.ts
@@ -1,8 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ZXingScannerComponent, ZXingScannerModule  } from '@zxing/ngx-scanner';
+import { ZXingScannerModule  } from '@zxing/ngx-scanner';
 
 import { FormsModule } from "@angular/forms";
+import {TicketService} from "../../providers/ticket.service";
+import {Ticket} from "../../interfaces/ticket.model";
 
 
 @Component({
@@ -10,23 +12,49 @@ import { FormsModule } from "@angular/forms";
   standalone: true,
   imports: [CommonModule, ZXingScannerModule, FormsModule],
   template: `
-      <zxing-scanner (scanSuccess)="onScanSuccess($event)">
-      </zxing-scanner>
-      @if(uuid) {
-        <h1>UUID: {{ uuid }}</h1>
+
+    @if(isLoading){
+      <img class="justify-center items-center" src="/assets/img/spinner.gif" alt=""/>
+    }
+
+    @if(!isLoading) {
+      @if(!ticket){
+        <div>
+          <zxing-scanner (scanSuccess)="onScanSuccess($event)">
+          </zxing-scanner>
+        </div>
       }
+
+      @if(ticket) {
+        <!-- Información del ticket vendido-->
+        <h1>{{ ticket | json }}</h1>
+        <button (click)="redeem(ticket)"
+                class="mt-5 flex w-full justify-center rounded bg-primary px-4 py-2 font-bold text-white drop-shadow hover:bg-success-dark"
+        >Redimir ticket</button>
+        <button (click)="ticket = undefined"
+                class="mt-5 flex w-full justify-center rounded bg-primary-dark px-4 py-2 font-bold text-white drop-shadow hover:bg-success-dark"
+        >Escanear de nuevo</button>
+      }
+    }
   `,
   styleUrl: './ticket-redeem.component.scss'
 })
 export class TicketRedeemComponent {
 
-  uuid: string = '';
+  isLoading = false;
+  ticket: Ticket | undefined;
+  ticketService = inject(TicketService);
 
-onScanSuccess($event: string) {
-  console.log($event);
-  alert($event);
-  this.uuid = $event;
-}
+  onScanSuccess(uuid: string) {
+    this.isLoading = true;
+    this.ticketService.getTicketByUUID(uuid).subscribe((result) => {
+      this.isLoading = false;
+      this.ticket = result;
+    })
+  }
 
+  redeem(ticket: Ticket){
+    throw new Error('Método no implementado!')
+  }
 
 }
